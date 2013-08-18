5bbbccbbd8de636a943ef8cb8d9c981b832e8fd6
keisatou <dummy>
2013-08-18 19:05:40 +0900

    changed location of dotfile directory.

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,1 +1,1 @@
-for i in `find ${HOME}/Dropbox/Public/dotfile -name '.*' -maxdepth 1`; do base=`basename $i`; cmd="ln -s $i ${HOME}/${base}"; [ ! -e ${HOME}/${base} ] && echo $cmd && $cmd; done
+for i in `find ${HOME}/Dropbox/dotfile -name '.*' -maxdepth 1`; do base=`basename $i`; cmd="ln -s $i ${HOME}/${base}"; [ ! -e ${HOME}/${base} ] && echo $cmd && $cmd; done
