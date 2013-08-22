9986b7dc66b6e11621792b17bc58a6fb81ec42b0
佐藤 啓一 <keisatou@satou-no-MacBook-Air.local>
2013-08-22 20:53:25 +0900

    renamed dot files

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,1 +1,21 @@
-for i in `find ${HOME}/Dropbox/dotfile -name '.*' -maxdepth 1`; do base=`basename $i`; cmd="ln -s $i ${HOME}/${base}"; [ ! -e ${HOME}/${base} ] && echo $cmd && $cmd; done
+#!/bin/sh
+
+DROPBOX_DIR="$HOME/Dropbox"
+DOTFILES="$DROPBOX_DIR/dotfiles"
+
+if [ ! -d "$DOTFILES" ]
+then
+    echo "Please create dotfiles to $DROPBOX_DIR"
+    exit 1
+fi
+
+cd $DOTFILES
+
+ln -s $DOTFILES/vimrc          $HOME/.vimrc
+ln -s $DOTFILES/vim            $HOME/.vim
+ln -s $DOTFILES/tmux.conf      $HOME/.tmux.conf
+ln -s $DOTFILES/inputrc        $HOME/.inputrc
+ln -s $DOTFILES/gitconfig      $HOME/.gitconfig
+ln -s $DOTFILES/chef           $HOME/.chef
+ln -s $DOTFILES/bash_profile   $HOME/.bash_profile
+ln -s $DOTFILES/bashrc         $HOME/.bashrc
