dc5ff362bba4a3eb8cdaf0e93e8360da295fc5a7
keisatou <dummy>
2013-11-12 01:27:16 +0900

    add platform detection (Darwin or Linux).

--- init.sh
+++ init.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Platform specific settings.
+PLATFORM=$(uname)
+case "$PLATFORM" in
+    'Darwin')
+        # Mac OSX
+        OPT_LN_NOTRACK='h' # Do not track symlink.
+        ;;
+    'Linux')
+        # CentOS etc.
+        OPT_LN_NOTRACK='n' # Do not track symlink.
+        ;;
+    *)
+        echo "unsupported OS"
+        exit 1
+        ;;
+esac
+
 # get the directory name the dotfiles are in.
 # (on the assumption that dot files are in the same directory as this script.)
 # this way you can git-clone dotfile directory anywhere you like :)
@@ -15,14 +32,14 @@ fi
 git submodule update --init
 git submodule foreach 'git checkout master'
 
-ln -sf      "$DOTFILES/vimrc"          "$HOME/.vimrc"
-ln -sfh     "$DOTFILES/vim"            "$HOME/.vim"
-ln -sf      "$DOTFILES/tmux.conf"      "$HOME/.tmux.conf"
-ln -sf      "$DOTFILES/inputrc"        "$HOME/.inputrc"
-ln -sf      "$DOTFILES/gitconfig"      "$HOME/.gitconfig"
-ln -sfh     "$DOTFILES/chef"           "$HOME/.chef"
-ln -sf      "$DOTFILES/bash_profile"   "$HOME/.bash_profile"
-ln -sf      "$DOTFILES/bashrc"         "$HOME/.bashrc"
-ln -sf      "$DOTFILES/gitignore"      "$HOME/.gitignore"
+ln -sf                      "$DOTFILES/vimrc"          "$HOME/.vimrc"
+ln -sf${OPT_LN_NOTRACK}     "$DOTFILES/vim"            "$HOME/.vim"
+ln -sf                      "$DOTFILES/tmux.conf"      "$HOME/.tmux.conf"
+ln -sf                      "$DOTFILES/inputrc"        "$HOME/.inputrc"
+ln -sf                      "$DOTFILES/gitconfig"      "$HOME/.gitconfig"
+ln -sf${OPT_LN_NOTRACK}     "$DOTFILES/chef"           "$HOME/.chef"
+ln -sf                      "$DOTFILES/bash_profile"   "$HOME/.bash_profile"
+ln -sf                      "$DOTFILES/bashrc"         "$HOME/.bashrc"
+ln -sf                      "$DOTFILES/gitignore"      "$HOME/.gitignore"
 
 vim -c ':NeoBundleInstall!'
